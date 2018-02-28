8beed9ea6c90ae56c534bc80872e158804502183
Miguel Angel Munoz <magonzalez@fortinet.com>
2018-02-28 10:02:02 +0100

    WIP: Try to use cloud unit with FAD

--- fortiadc/start_fad.sh
+++ fortiadc/start_fad.sh
@@ -69,23 +69,14 @@ mkdir -p cfg-drv-fgt/openstack/latest/
 mkdir -p cfg-drv-fgt/openstack/content/
 
 cat >cfg-drv-fgt/openstack/content/0000 <<EOF
------BEGIN FGT VM LICENSE-----
-<empty> Put your license here
------END FGT VM LICENSE-----
-EOF
-
-cat >cfg-drv-fgt/openstack/latest/user_data <<EOF
 config system interface
   edit "port1"
     set mode static
     set ip 192.168.122.40/24
-    set allowaccess https ping ssh snmp http telnet fgfm radius-acct probe-response capwap ftm
+    set allowaccess https ping ssh snmp http telnet
   next
 end
-config system dns
-  set primary 8.8.8.8
-  set secondary 8.8.4.4
-end
+
 config router static
   edit 2
     set gateway 192.168.122.1
@@ -94,10 +85,28 @@ config router static
 end
 EOF
 
-sudo mkisofs -publisher "OpenStack Nova 12.0.2" -J -R -V config-2 -o ${FAD_NAME}-cidata.iso cfg-drv-fgt
+cat >cfg-drv-fgt/openstack/latest/user_data <<EOF
+config system interface
+  edit "port1"
+    set mode static
+    set ip 192.168.122.40/24
+    set allowaccess https ping ssh snmp http telnet
+  next
+end
+
+config router static
+  edit 2
+    set gateway 192.168.122.1
+    set device "port1"
+  next
+end
+EOF
+
+sudo mkisofs -publisher "OpenStack Nova 12.0.2" -J -R -V config-2 -o ${FAD_NAME}-cidata.iso cfg-drv-fgt/
 virt-install --connect qemu:///system --noautoconsole --filesystem ${PWD},shared_dir --import --name ${FAD_NAME} \
 --ram 4096 --vcpus 1 --disk fortiadc.qcow2,size=3,bus=virtio --disk ${FORTIADC_DATA_QCOW2},bus=virtio \
 --network bridge=virbr0,mac=${FAD_MAC_ADMIN},model=virtio \
 --network bridge=virbr0,mac=${FAD_MAC},model=virtio \
 --network bridge=virbr0,mac=${FAD_MAC2},model=virtio \
---disk ${FAD_NAME}-cidata.iso,device=cdrom,bus=ide,format=raw,cache=none
+--disk ${FAD_NAME}-cidata.iso,device=cdrom,bus=ide,format=raw,cache=none \
+--os-variant fedora-unknown
