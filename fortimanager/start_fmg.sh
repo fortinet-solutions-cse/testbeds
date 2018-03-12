36bd5a92cd9d7c239d4cf6b070edac0806023e07
Miguel Angel Munoz <magonzalez@fortinet.com>
2018-03-12 09:02:41 +0100

    WIP: FortiManager scenario

--- fortimanager/start_fmg.sh
+++ fortimanager/start_fmg.sh
@@ -2,7 +2,7 @@
 
 #************************************************
 #
-# Use this script to start a FortiGate VM with
+# Use this script to start a FortiManager VM with
 # LibVirt, no VIM required.
 # This has support for cloud init, see below how
 # to build cdrom with proper content
@@ -12,52 +12,52 @@
 # ************************************************
 
 #************************************************
-# Check Fortigate VM existence
+# Check FortiManager VM existence
 #************************************************
 
 if [ -z "$1" ]; then
-  echo "Need location of Fortigate image"
+  echo "Need location of FortiManager image"
   exit -1
 fi
 result=$(file $1)
 if [[ $result == *"QEMU QCOW Image (v2)"* ]]; then
-   echo "Supplied Fortigate image is in: $1"
-   FORTIGATE_QCOW2=$1
+   echo "Supplied FortiManager image is in: $1"
+   FORTIMANAGER_QCOW2=$1
 else
-   echo "Supplied Fortigate image does not look a qcow2 file"
+   echo "Supplied FortiManager image does not look a qcow2 file"
    exit -1
 fi
-if [[ "$(realpath $FORTIGATE_QCOW2)" == "$(pwd)/fortios.qcow2" ]]; then
-   echo "FortiGate image can not be named fortios.qcow2 in this directory. Choose different location/name"
+if [[ "$(realpath $FORTIMANAGER_QCOW2)" == "$(pwd)/fortimanager.qcow2" ]]; then
+   echo "Fortimanager image can not be named fortimanager.qcow2 in this directory. Choose different location/name"
    exit -1
 fi
 
-export SF2_NAME=fortigate
-export SF2_IP_ADMIN=192.168.122.40
-export SF2_IP=192.168.70.40
-export SF2_IP2=192.168.80.40
-export SF2_MAC_ADMIN=08:00:27:4c:22:40
-export SF2_MAC=08:00:27:4c:70:40
-export SF2_MAC2=08:00:27:4c:80:40
+export SF_NAME=fortimanager
+export SF_IP_ADMIN=192.168.122.40
+export SF_IP=192.168.70.40
+export SF_IP2=192.168.80.40
+export SF_MAC_ADMIN=08:00:27:4c:22:40
+export SF_MAC=08:00:27:4c:70:40
+export SF_MAC2=08:00:27:4c:80:40
 
-rm -f fortios.qcow2
-rm -rf cfg-drv-fgt
-rm -rf ${SF2_NAME}-cidata.iso
+rm -f fortimanager.qcow2
+rm -rf cfg-drv-fmg
+rm -rf ${SF_NAME}-cidata.iso
 
-cp ${FORTIGATE_QCOW2} ./fortios.qcow2
+cp ${FORTIMANAGER_QCOW2} ./fortimanager.qcow2
 
-mkdir -p cfg-drv-fgt/openstack/latest/
-mkdir -p cfg-drv-fgt/openstack/content/
+mkdir -p cfg-drv-fmg/openstack/latest/
+mkdir -p cfg-drv-fmg/openstack/content/
 
-cat >cfg-drv-fgt/openstack/content/0000 <<EOF
------BEGIN FGT VM LICENSE-----
+cat >cfg-drv-fmg/openstack/content/0000 <<EOF
+-----BEGIN FMG VM LICENSE-----
 <empty> Put your license here
------END FGT VM LICENSE-----
+-----END FMG VM LICENSE-----
 EOF
 
-cat >cfg-drv-fgt/openstack/latest/user_data <<EOF
+cat >cfg-drv-fmg/openstack/latest/user_data <<EOF
 config system interface
-  edit "port1"
+  edit "port1"license
     set vdom "root"
     set mode static
     set ip 192.168.122.40/24
@@ -79,5 +79,14 @@ config system global
 end
 EOF
 
-sudo mkisofs -publisher "OpenStack Nova 12.0.2" -J -R -V config-2 -o ${SF2_NAME}-cidata.iso cfg-drv-fgt
-virt-install --connect qemu:///system --noautoconsole --filesystem ${PWD},shared_dir --import --name ${SF2_NAME} --ram 1024 --vcpus 1 --disk fortios.qcow2,size=3 --disk fgt-logs.qcow2,size=3 --disk ${SF2_NAME}-cidata.iso,device=cdrom,bus=ide,format=raw,cache=none --network bridge=virbr0,mac=${SF2_MAC_ADMIN},model=virtio
+sudo mkisofs -publisher "OpenStack Nova 12.0.2" -J -R -V config-2 -o ${SF_NAME}-cidata.iso cfg-drv-fmg
+virt-install --connect qemu:///system --noautoconsole --filesystem ${PWD},shared_dir --import \
+  --name ${SF_NAME} --ram 4096 --vcpus 2 --disk fortimanager.qcow2,size=3 \
+  --disk ${SF_NAME}-cidata.iso,device=cdrom,bus=ide,format=raw,cache=none \
+  --network bridge=virbr0,mac=${SF_MAC_ADMIN},model=virtio
+#  --disk fmg-logs.qcow2,size=30
+
+
+# config system admin setting
+# set shell-access enable
+# execute shell
