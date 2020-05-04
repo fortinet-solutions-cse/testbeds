4a983ebba9dad5223979e454c59b70826f6a72d8
Miguel Muñoz <magonzalez@fortinet.com>
2020-05-04 16:06:32 +0200

    Updates for Ubuntu 20.04

--- installation.sh
+++ installation.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-#To be done in ubuntu 16.04.2LTS
+#To be done in ubuntu 20.04LTS
 
-sudo apt update
-sudo apt-get install -y  openjdk-8-jdk
-sudo apt-get install -y qemu-kvm libvirt-bin libguestfs-tools virtinst
-sudo apt-get install -y sshpass wget curl openssh-client xterm
+sudo apt install -y virtinst virt-manager
 
 #Manual
 #Generate public key in default dir (~/.ssh/id_rsa.pub)
 
-#Optional
-sudo apt-get install -y virt-manager
 
 # Things done
 # Check this: set etc/sysctl.conf: ipv4.forward (uncomment)
