ce0b9f61936460cd91482108b87fc927a5b4f598
Miguel Muñoz <magonzalez@fortinet.com>
2020-05-08 18:51:27 +0200

    Cloud Init with FMG

--- fortimanager/start_fmg.sh
+++ fortimanager/start_fmg.sh
@@ -44,9 +44,42 @@ rm -rf ${SF_NAME}-cidata.iso
 
 cp ${FORTIMANAGER_QCOW2} ./fortimanager.qcow2
 
+cp ${FORTIGATE_QCOW2} ./fortios.qcow2
+
+mkdir -p cfg-drv-fgt/openstack/latest/
+mkdir -p cfg-drv-fgt/openstack/content/
+
+cat >cfg-drv-fgt/openstack/content/0000 <<EOF
+-----BEGIN FMG VM LICENSE-----
+<empty> Put your license here
+-----END FMG VM LICENSE-----
+EOF
+
+#v FGT 6.2.3
+
+cat >cfg-drv-fgt/openstack/latest/user_data <<EOF
+config system interface
+  edit "port1"
+    set ip ${SF_IP_ADMIN}/24
+    set allowaccess http https ping snmp ssh webservice
+  next
+end
+config system admin user
+  edit admin
+    set password m
+  next
+end
+config system global
+    set hostname FMG-VM64-KVM
+end
+EOF
+
+sudo mkisofs -publisher "OpenStack Nova 12.0.2" -J -R -V config-2 -o ${SF_NAME}-cidata.iso cfg-drv-fgt
+
 virt-install --connect qemu:///system --noautoconsole --filesystem ${PWD},shared_dir --import \
   --cpu host,-vmx \
   --name ${SF_NAME} --ram 4096 --vcpus 2 --disk fortimanager.qcow2,size=3 \
+  --disk ${SF_NAME}-cidata.iso,device=cdrom,bus=ide,format=raw,cache=none \
   --network bridge=virbr0,mac=${SF_MAC_ADMIN},model=virtio
 #  --disk fmg-logs.qcow2,size=30
 
