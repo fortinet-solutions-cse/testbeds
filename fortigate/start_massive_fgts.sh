c741af02ab082a9ca55c5abc8dbd8a99596a1932
Miguel Muñoz <magonzalez@fortinet.com>
2020-10-01 16:46:02 +0200

    Apply config massively

--- fortigate/start_massive_fgts.sh
+++ fortigate/start_massive_fgts.sh
@@ -52,22 +52,21 @@ server_id=$(hostname|grep -o [0-9])
 
 sudo virsh net-destroy mpls_a
 sudo virsh net-undefine mpls_a
+
+sudo ip link add name mpls_a type bridge
+sudo ip link set mpls_a up
+
 cat >mpls_a <<EOF
 <network>
   <name>mpls_a</name>
-  <forward mode='route'>
-  </forward>
-  <bridge name='mpls_a' stp='off' delay='0'/>
-  <mac address='52:54:00:79:7c:c3'/>
-  <ip address='172.16.${server_id}.1' netmask='255.255.255.0'>
-    <dhcp>
-      <range start='172.16.${server_id}.2' end='172.16.${server_id}.254'/>
-    </dhcp>
-  </ip>
+  <forward mode='bridge'/>
+  <bridge name='mpls_a'/>
 </network>
 EOF
-sudo virsh net-create mpls_a
+sudo virsh net-define mpls_a
+sudo virsh net-start mpls_a
 sudo virsh net-autostart mpls_a
+sudo ip link set em1 master mpls_a
 
 
 ################################
@@ -120,7 +119,7 @@ mkdir -p cfg-drv-fgt/openstack/content/
 
 install()
 {
-    for i in {2..3}
+    for i in {2..251}
 #    for i in 242 249 250 251
     do
         echo $i
@@ -340,5 +339,240 @@ check_license()
 
 }
 
+
+run_ssh_massively()
+{
+server_id=$(hostname|grep -o [0-9])
+SF_IP_BASE=192.168.122
+for i in {2..251}
+do
+echo $i
+sshpass -pm ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null admin@192.168.122.${i} "config system interface
+edit port1
+set vdom root
+set mode static
+set ip ${SF_IP_BASE}.${i}/24
+set allowaccess ping https ssh snmp http telnet fgfm radius-acct probe-response fabric ftm 
+set mtu-override enable
+set mtu 1460
+next
+edit m1v1
+set vdom root
+set type vlan
+set ip 172.11.${server_id}.${i} 255.255.0.0
+set allowaccess ping https ssh http fgfm      
+set interface port2
+set vlanid 11
+next
+edit m1v2
+set vdom root
+set type vlan
+set ip 172.12.${server_id}.${i} 255.255.0.0
+set allowaccess ping
+set interface port2
+set vlanid 12
+next
+edit m1v3
+set vdom root
+set type vlan
+set ip 172.13.${server_id}.${i} 255.255.0.0
+set allowaccess ping
+set interface port2
+set vlanid 13
+next
+edit m1v4
+set vdom root
+set type vlan
+set ip 172.14.${server_id}.${i} 255.255.0.0
+set allowaccess ping
+set interface port2
+set vlanid 14
+next
+edit m1v5
+set vdom root
+set type vlan
+set ip 172.15.${server_id}.${i} 255.255.0.0
+set allowaccess ping
+set interface port2
+set vlanid 15
+next
+edit m1v6
+set vdom root
+set type vlan
+set ip 172.16.${server_id}.${i} 255.255.0.0
+set allowaccess ping
+set interface port2
+set vlanid 16
+next
+edit m1v7
+set vdom root
+set type vlan
+set ip 172.17.${server_id}.${i} 255.255.0.0
+set allowaccess ping
+set interface port2
+set vlanid 17
+next
+edit m1v8
+set vdom root
+set type vlan
+set ip 172.18.${server_id}.${i} 255.255.0.0
+set allowaccess ping
+set interface port2
+set vlanid 18
+next 
+edit m2v1
+set vdom root
+set type vlan
+set ip 172.21.${server_id}.${i} 255.255.0.0
+set interface port3
+set vlanid 21
+next
+edit m2v2
+set vdom root
+set type vlan
+set ip 172.22.${server_id}.${i} 255.255.0.0
+set allowaccess ping
+set interface port3
+set vlanid 22
+next
+edit m2v3
+set vdom root
+set type vlan
+set ip 172.23.${server_id}.${i} 255.255.0.0
+set allowaccess ping
+set interface port3
+set vlanid 23
+next
+edit m2v4
+set vdom root
+set type vlan
+set ip 172.24.${server_id}.${i} 255.255.0.0
+set allowaccess ping
+set interface port3
+set vlanid 24
+next
+edit m2v5
+set vdom root
+set type vlan
+set ip 172.25.${server_id}.${i} 255.255.0.0
+set allowaccess ping
+set interface port3
+set vlanid 25
+next 
+edit m2v6
+set vdom root
+set type vlan
+set ip 172.26.${server_id}.${i} 255.255.0.0
+set allowaccess ping
+set interface port3
+set vlanid 26
+next
+edit m2v7
+set vdom root
+set type vlan
+set ip 172.27.${server_id}.${i} 255.255.0.0
+set allowaccess ping
+set interface port3
+set vlanid 27
+next
+edit m2v8
+set vdom root
+set type vlan
+set ip 172.28.${server_id}.${i} 255.255.0.0
+set allowaccess ping
+set interface port3
+set vlanid 28
+next  
+edit vl_v1
+set vdom root
+set type emac-vlan
+set vrf 1
+set ip 10.${server_id}01.${i}.1 255.255.255.0
+set allowaccess ping
+set interface port4
+next
+edit vl_v2
+set vdom root
+set type emac-vlan
+set vrf 2
+set ip 10.${server_id}02.${i}.1 255.255.255.0
+set allowaccess ping
+set interface port4
+next
+edit vl_v3
+set vdom root
+set type emac-vlan
+set vrf 3
+set ip 10.${server_id}03.${i}.1 255.255.255.0
+set allowaccess ping
+set interface port4
+next
+edit vl_v4
+set vdom root
+set type emac-vlan
+set vrf 4
+set ip 10.${server_id}04.${i}.1 255.255.255.0
+set allowaccess ping
+set interface port4
+next
+edit vl_v5
+set vdom root
+set type emac-vlan
+set vrf 5
+set ip 10.${server_id}05.${i}.1 255.255.255.0
+set allowaccess ping
+set interface port4
+next
+edit vl_v6
+set vdom root
+set type emac-vlan
+set vrf 6
+set ip 10.${server_id}06.${i}.1 255.255.255.0
+set allowaccess ping
+set interface port4
+next
+edit vl_v7
+set vdom root
+set type emac-vlan
+set vrf 7
+set ip 10.${server_id}07.${i}.1 255.255.255.0
+set allowaccess ping
+set interface port4
+next
+edit vl_v8
+set vdom root
+set type emac-vlan
+set vrf 8
+set ip 10.${server_id}08.${i}.1 255.255.255.0
+set allowaccess ping
+set interface port4
+next  
+end
+config system dns
+set primary 8.8.8.8
+set secondary 8.8.4.4
+end
+config router static
+edit 2
+set gateway 192.168.122.1
+set device port1
+next
+end
+config system global
+set admintimeout 480
+set hostname fgt${i}
+end
+config system admin
+edit admin
+set password m
+next
+end
+"
+        
+done
+
+}
+
+
 install
-# destroy 
+#destroy
