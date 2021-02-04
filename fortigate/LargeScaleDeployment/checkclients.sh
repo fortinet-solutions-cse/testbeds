a98d681c29df411a9ec1d268314aad8316039966
Nicolas Thomas <thomnico@googlemail.com>
2021-02-04 14:04:58 +0100

    make checkclients output easier to understand and summarize the ones with issues

--- fortigate/LargeScaleDeployment/checkclients.sh
+++ fortigate/LargeScaleDeployment/checkclients.sh
@@ -7,20 +7,22 @@ if [[ $# -ne 1 ]]
   (echo "must give N "; exit 2)
  fi
 export N=$1
-
-for m in {1..250} 
-do 
+export RESTARTED=""
+for m in {1..250}
+do
 echo "check client-$N-$m:"
- if docker top client-$N-$m 
+ if docker top client-$N-$m >/dev/null
  then
  echo "client-$N-$m is running"
  else
  echo "client-$N-$m stop/rebuild"
  docker stop client-$N-$m || true
- docker rm -f client-$N-$m 
+ docker rm -f client-$N-$m
 # virsh reboot site-$N-$m
  sleep 3
  docker run --net=c$N-$m --ip 10.$N.$m.5 -itd --name client-$N-$m --cpu-shares 200 --cpus 0.1 --memory 0.1G  fit
+ RESTARTED="$RESTARTED $m"
  fi
 done
+echo "client with network issues: $RESTARTED"
 
