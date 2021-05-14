1bc92f3eddd8437375e9d16358598a3eb3221a6f
Miguel A. Munoz <magonzalez@fortinet.com>
2021-05-14 09:50:55 +0200

    New use cases including token regeneration for FlexVM

--- LargeScaleDeployment/fortigate/flex_vm_license.py
+++ LargeScaleDeployment/fortigate/flex_vm_license.py
@@ -4,7 +4,7 @@ import requests
 import json
 import collections
 
-bearer = 'add_your_bearer_here'
+bearer = 'your_own_bearer_token'
 
 # Create new licenses for VMs
 # Parameters:
@@ -52,17 +52,18 @@ def update_vm_license(serialNumber, configId, description, endDate):
 
 
 # Obtain list of licenses for VMs
+# for a particular configuration
 # Returns a list with each item being a dict with:
 #   serialNumber
 #   configId
 #   description
 #   endDate
 #
-def get_vms_licenses():
+def get_vms_licenses(configId):
   url = "https://support.fortinet.com/ES/api/flexvm/v1/vms/list"
 
   payload = json.dumps({
-      "configId": 257
+      "configId": configId
   })
   headers = {
     'Authorization': 'Bearer ' + bearer,
@@ -70,7 +71,7 @@ def get_vms_licenses():
   }
 
   response = requests.request("POST", url, headers=headers, data=payload)
-  #print(response.text)
+  print(response.text)
   vms = response.json()
 
   return vms['result']
@@ -100,17 +101,39 @@ def calculateNM(N,M):
   return N, M
 
 
+def regenerateToken(serialNumber):
+  url = "https://support.fortinet.com/ES/api/flexvm/v1/vms/token"
+
+  payload = json.dumps({
+      "serialNumber": serialNumber
+  })
+  headers = {
+    'Authorization': 'Bearer ' + bearer,
+    'Content-Type': 'application/json'
+  }
+
+  response = requests.request("POST", url, headers=headers, data=payload)
+  print(response.text)
+
+
+######################################################
 # Examples
+######################################################
 
-# USE CASE 1: Create massive amount of licenses
 
+
+# # USE CASE 1: Create massive amount of licenses
+######################################################
+#
 # for i in range(0,23):
 #  create_vm_license(257, 30, "LSD2", None)
 
 
-# USE CASE 2: Simple update
 
-# vms = get_vms_licenses()
+# # USE CASE 2: Simple update
+######################################################
+#
+# vms = get_vms_licenses(257)
 # for vm in vms:
 #     if vm['serialNumber'] > 'FGVMMLTM21002841'  and vm['serialNumber']<='FGVMMLTM21003095':
 #       print(vm['serialNumber'] + " " + str(vm['description']))
@@ -119,19 +142,76 @@ def calculateNM(N,M):
 #         update_vm(vm['serialNumber'], 257, 'LSD2', None)
 
 
-if __name__ == 'main':
 
-  # USE CASE 3: Rename each VM with a proper site-id name
-  #
-  vms = get_vms_licenses()
-  vms.sort(key=getSN)
+# # USE CASE 3: Rename each VM with a proper site-id name
+######################################################
+# #
+# vms = get_vms_licenses()
+# vms.sort(key=getSN)
 
-  N=1
-  M=1
-  vm_number = 0
-  for vm in vms:
-    print(f" number: {vm_number} key: {vm['serialNumber']} token: {vm['token']} N: {N}  M:{M}")
-    update_vm_license(vm['serialNumber'], 257, f'site-{N}-{M}', None)
-    vm_number +=1
-    N, M = calculateNM(N,M)
+# N=1
+# M=1
+# vm_number = 0
+# for vm in vms:
+#   print(f" number: {vm_number} key: {vm['serialNumber']} token: {vm['token']} N: {N}  M:{M}")
+#   update_vm_license(vm['serialNumber'], 257, f'site-{N}-{M}', None)
+#   vm_number +=1
+#   N, M = calculateNM(N,M)
+
+
+
+# # USE CASE 4: Rename each VM with a proper site-id name only for a subset of licenses
+######################################################
+#  (only those higher than a particular serialNumber)
+# #
+# vms = get_vms_licenses(257)
+# vms.sort(key=getSN)
+
+# N=1
+# M=1
+# vm_number = 0
+# for vm in vms:
+#   if vm['serialNumber']>='FGVMMLTM21005559':
+#     print(f" number: {vm_number} key: {vm['serialNumber']} token: {vm['token']} N: {N}  M:{M}")
+#     update_vm_license(vm['serialNumber'], 257, f'site-{N}-{M}', None)
+#   else:
+#     print(f" skipping {vm_number} with description {vm['description']}")
+#   vm_number +=1
+#   N, M = calculateNM(N,M)
+
+
+
+# # USE CASE 5: Regenerate tokens for all licenses
+# ######################################################
+# #
+# vms = get_vms_licenses(257)
+# vms.sort(key=getSN)
+
+# N=1
+# M=1
+# vm_number = 0
+# for vm in vms:
+#   print(f" number: {vm_number} key: {vm['serialNumber']} token: {vm['token']} N: {N}  M:{M}")
+#   regenerateToken(vm['serialNumber'])
+#   vm_number +=1
+#   N, M = calculateNM(N,M)
+
+
+
+# USE CASE 6: List all VMs with relevant info
+######################################################
+#
+# vms = get_vms_licenses(257)
+# vms.sort(key=getSN)
+
+# for vm in vms:
+#   print(f" number: {vm_number} key: {vm['serialNumber']} token: {vm['token']} description: {vm['description']} Status: {vm['tokenStatus']}")
+
+if __name__ == '__main__':
+
+# copy paste one of the previous use cases as a baseline for your own development
+# or simply use the functions to your convenience.
+
+
+
 
